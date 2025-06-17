1d0420a27f212dad2a4dc6d273a30bbcff191b5b
Ines Lalou <58553243+InesLalou@users.noreply.github.com>
2025-06-17 14:26:37 +0200

    Add files via upload

---
+++ Hello_Word.py
@@ -0,0 +1,7 @@
+from calcul import calcul_valeur
+
+# On appelle la fonction pour obtenir la valeur
+valeur = calcul_valeur()
+
+# On affiche le résultat
+print("Le résultat du calcul est :", valeur)
---
+++ calcul.py
@@ -0,0 +1,4 @@
+def calcul_valeur():
+    # Exemple : on calcule la somme de 2 et 3
+    resultat = 2 + 3
+    return resultat
